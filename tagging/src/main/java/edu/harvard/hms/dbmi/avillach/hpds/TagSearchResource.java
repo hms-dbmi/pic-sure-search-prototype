f19790571fa2b24b6641b0f540cfdd751e6d2c6a
James <Jamestp19@gmail.com>
2022-06-22 08:15:48 -0400

    ALS-3188: Allows tags with 1 result

--- tagging/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/TagSearchResource.java
+++ tagging/src/main/java/edu/harvard/hms/dbmi/avillach/hpds/TagSearchResource.java
@@ -96,8 +96,7 @@ public class TagSearchResource implements IResourceRS {
                     }).sorted(Comparator.comparing(TagResult::getScore).reversed());
             if(tagStats.size()>10) {
                 tagResults = tagResults.filter(result -> 
-                result.getScore() > 1 && (
-                result.getTag().toLowerCase().matches("dcc harmonized data set") ||
+                (result.getTag().matches("DCC Harmonized data set") ||
         		result.getTag().matches("PHS\\d{6}+.*") || 
         		result.getTag().toUpperCase().matches("PHT\\d{6}+.*") || 
                 	(result.getScore() > numVars * .05 && result.getScore() < numVars * .95)
